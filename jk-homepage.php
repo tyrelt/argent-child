637cfde5dc546afe9d9e2ef441252e3696209198
tyrelt <tyrel.tjoelker@gmail.com>
2016-04-08 16:44:29 -0700

    Replace lorem ipsum with Advanced Custom Fields and update colors

--- jk-homepage.php
+++ jk-homepage.php
@@ -16,10 +16,8 @@ get_header(); ?>
 			<section class="hero" style="background-image: url('<?php the_field('hero_bg'); ?>');">
 				<div class="contentContainer">
 					<div class="heroText">
-						<h1>$300,000 in 6 months.</h1>
-						<h2>But that’s not really the point.</h2>
-						<p>Ipsum corrupti nisi quam atque provident libero quos assumenda blanditiis, laudantium consequuntur, dignissimos, rerum itaque modi aperiam unde. Beatae ab facere aut, dicta accusantium alias ut magni consequuntur! Beatae harum, sunt voluptas?</p>
-						<a class="primaryBtn fullBtn" href="feature">Click here</a>
+						<?php the_field('hero_text') ?>
+						<a class="primaryBtn fullBtn" href="<?php the_field('hero_button_link') ?>"><?php the_field('hero_button_text') ?></a>
 					</div>
 				</div>
 			</section>
@@ -27,43 +25,33 @@ get_header(); ?>
 			<!-- Trust marks -->
 			<section class="trustmarks">
 				<div class="contentContainer">
-					<h1 class="title"><span>I’ve been featured in cool places</span></h1>
-					<!-- <h2 class="sectionHeader"><?php the_field('clients_header'); ?></h2> -->
+					<h1 class="title"><span><?php the_field('trustmarks_header') ?></span></h1>
 				    <ul>
-				        <li><a href="<?php the_field('client1_link'); ?>" target="_blank"><img src="<?php the_field('client1_image'); ?>" alt="<?php the_field('client1_alt'); ?>"></a></li>
-				        <li><a href="<?php the_field('client2_link'); ?>" target="_blank"><img src="<?php the_field('client2_image'); ?>" alt="<?php the_field('client2_alt'); ?>"></a></li>
-				        <li><a href="<?php the_field('client3_link'); ?>" target="_blank"><img src="<?php the_field('client3_image'); ?>" alt="<?php the_field('client3_alt'); ?>"></a></li>
+				        <li><a href="<?php the_field('image1_link'); ?>" target="_blank"><img src="<?php the_field('image1'); ?>" alt="<?php the_field('image1_alt'); ?>"></a></li>
+				        <li><a href="<?php the_field('image2_link'); ?>" target="_blank"><img src="<?php the_field('image2'); ?>" alt="<?php the_field('image2_alt'); ?>"></a></li>
+				        <li><a href="<?php the_field('image3_link'); ?>" target="_blank"><img src="<?php the_field('image3'); ?>" alt="<?php the_field('image3_alt'); ?>"></a></li>
 				    </ul>
 				</div>
 			</section>
 
 			<!-- single column for benefits -->
 			<section class="singleColBenefits">
-				<div class="benefitsOverview" style="background-image: url('<?php the_field('benefits_overview'); ?>');">
-					<h1>Here&rsquo;s another placeholder h1</h1>
-					<h3>And a bit of descriptive text for underneath it</h3>
+				<div class="benefitsOverview" style="background-image: url('<?php the_field('benefits_bg'); ?>');">
+					<?php the_field('benefits_header') ?>
 				</div>
 				<div class="split">
-					<div class="halfImage" style="background-image: url('<?php the_field('50_bg'); ?>');"></div>
+					<div class="halfImage" style="background-image: url('<?php the_field('half_bg'); ?>');"></div>
 					<div class="halfText">
 						<div class="halfTextContent">
 							<div class="halfTextContentInner">
-								<h3>Lorem ipsum dolor sit amet.</h3>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur.</p>
+								<?php the_field('half_text') ?>
 							</div>
 						</div>
 					</div>
 				</div>
 				<div class="contentContainer">
 					<div class="twoColumn">
-						<h3>Benefit one</h3>
-						<p>Here's some text</p>
-						<h3>Lorem ipsum dolor sit.</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, porro!</p>
-						<h3>Lorem ipsum dolor sit amet.</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur.</p>
-						<h3>Lorem ipsum dolor sit.</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+						<?php the_field('benefits') ?>
 					</div>
 				</div>
 				<span class="triangle"></span>
@@ -72,8 +60,8 @@ get_header(); ?>
 			<!-- Blog preview section -->
 			<section class="blogPreview">
 				<div class="contentContainer">
-					<h1 class="title"><span>Here's something to read</span></h1>
-					<h3>So you're not a bum</h3>
+					<h1 class="title"><span><?php the_field('postpreview_header') ?></span></h1>
+					<?php the_field('postpreview_secondary_header')?>					
 					<article class="postContainer">
 								
 						<?php
@@ -97,7 +85,7 @@ get_header(); ?>
 								<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 								<?php the_post_thumbnail('large', array( 'class' => "postPreviewThumb")); ?>		
 						
-								<?php the_content('<br><span class="primaryBtn postBtn">Keep reading</span>'); ?>
+								<?php the_content('<br><span class="primaryBtn postBtn">Continue reading</span>'); ?>
 							</div>
 						<?php endforeach; ?>
 						<?php wp_reset_query() ?>
@@ -105,7 +93,7 @@ get_header(); ?>
 				</div>
 				<div class="ctaOuter" style="background-image: url('<?php the_field('cta_bg'); ?>');">
 					<div class="ctaInner">
-						<p><a class="primaryBtn fullBtn" href="">See all the posts</a></p>
+						<p><a class="primaryBtn fullBtn" href="<?php the_field('full_button_link') ?>"><?php the_field('full_button') ?></a></p>
 					</div>
 				</div>
 			</section>
